9801d6295461e1e6eaf92389f948a1392d3cda23
raksha1207 <30931862+raksha1207@users.noreply.github.com>
2020-09-19 19:29:11 -0400

    Create gits_pr_update.py

---
+++ code/gits_pr_update.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import subprocess
+import argparse
+
+
+def gits_pr_update(args):
+
+
+        #print(args)
+        #print("Hello from GITS command line tools- PR Update")
+        #subprocess.call(["git checkout master"],shell=True)
+        #subprocess.call(["git pull upstream master"], shell = True)
+
+
+
